ca4c6a2e81ac1cdb5ab241be5f10762f730b1e15
root <root@localhost.localdomain>
2019-12-23 00:31:11 +0100

    removal of unused function

--- utils/io/keyboard.c
+++ utils/io/keyboard.c
@@ -60,20 +60,3 @@ char* read_line(){
 
     return buf;
 }
-
-
-void test_input()
-{
-  char ch = 0;
-  char keycode = 0;
-  do{
-    keycode = get_input_keycode();
-    if(keycode == KEY_ENTER){
-      print_new_line();
-    }else{
-      ch = get_ascii_char(keycode);
-      print_char(ch);
-    }
-    sleep(0x02FFFFFF);
-  }while(ch > 0);
-}
