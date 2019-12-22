262fed1998f2c0c01d6de0fcd8d76909fdb3a061
root <root@localhost.localdomain>
2019-12-23 00:28:30 +0100

    cleanup

--- run.sh
+++ run.sh
@@ -5,16 +5,16 @@ as --32 boot.s -o boot.o
 gcc -m32 -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
 #linking the kernel with kernel.o and boot.o files
-ld -m elf_i386 -T linker.ld kernel.o boot.o -o MyOS.bin -nostdlib
+ld -m elf_i386 -T linker.ld kernel.o boot.o -o nailsOS.bin -nostdlib
 
 #check MyOS.bin file is x86 multiboot file or not
-grub-file --is-x86-multiboot MyOS.bin
+grub-file --is-x86-multiboot nailsOS.bin
 
 #building the iso file
 mkdir -p isodir/boot/grub
-cp MyOS.bin isodir/boot/MyOS.bin
+cp nailsOS.bin isodir/boot/nailsOS.bin
 cp grub.cfg isodir/boot/grub/grub.cfg
-grub-mkrescue -o MyOS.iso isodir
+grub-mkrescue -o nailsOS.iso isodir
 
 #run it in qemu
-qemu-system-x86_64 -cdrom MyOS.iso
+qemu-system-x86_64 -cdrom nailsOS.iso
