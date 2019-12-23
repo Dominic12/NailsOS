ea68a758918ee7df8ac68e66f1fe32cb6d276e6b
root <root@localhost.localdomain>
2019-12-23 18:22:23 +0100

    Memory Pages comment

--- mem/mem.c
+++ mem/mem.c
@@ -1,5 +1,9 @@
 #include "mem.h"
 
+
+/*
+    Dump, yet unefficient way of managing memory. Certainly needs more attention in the future.
+*/
 void* lower_mem;
 void* upper_mem;
 Page* top_page; // Keep track of the Top page, (Performance)
@@ -18,7 +22,7 @@ Page* create_page(void* addr, size_t size){
     printf("Creating page at: 0x%p\n", addr);
     Page *page = (Page*)addr;
     page->size = size;
-    memset(page->rw_data, 0, sizeof(page->rw_data));
+    memset(page->rw_data, 0, sizeof(page->rw_data)); //Clear out arbitary data
     top_page = page;
     return page;
 }
